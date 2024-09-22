6664f68dbfc4a9e1e7d3358352ecc9d4e02f6e25
Roman Stolyarchuk <roman@stolyarch.uk>
2024-09-22 11:56:19 +0400

    refine MainContract: update configuration handling, enhance tests, and add deposit functionality

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -13,40 +13,80 @@ describe('main.fc contract tests', () => {
 
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
-    let main: SandboxContract<MainContact>;
+    let mainContract: SandboxContract<MainContact>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        const initAddress = await blockchain.treasury("initAddress");
+        const initWallet = await blockchain.treasury('initWallet');
+        const ownerWallet = await blockchain.treasury('ownerWallet');
 
-        main = blockchain.openContract(MainContact.createFromConfig({
-            number: 0,
-            address: initAddress.address,
-        }, code));
+        mainContract = blockchain.openContract(
+            await MainContact.createFromConfig(
+                {
+                    number: 0,
+                    address: initWallet.address,
+                    owner_address: ownerWallet.address,
+                },
+                code,
+            ),
+        );
 
         deployer = await blockchain.treasury('deployer');
 
-        const deployResult = await main.sendDeploy(deployer.getSender(), toNano('0.05'));
+        const deployResult = await mainContract.sendDeploy(deployer.getSender(), toNano('0.05'));
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
-            to: main.address,
+            to: mainContract.address,
             deploy: true,
             success: true,
         });
     });
 
     it('should incremnet', async () => {
-        const sentMessageResult = await main.sendIncrement(deployer.getSender(), toNano('0.05'), 1);
+        const sentMessageResult = await mainContract.sendIncrement(deployer.getSender(), toNano('0.05'), 1);
 
         expect(sentMessageResult.transactions).toHaveTransaction({
             from: deployer.address,
-            to: main.address,
+            to: mainContract.address,
             success: true,
         });
 
-        const data = await main.getData();
+        const data = await mainContract.getData();
         expect(data.recent_sender.toString()).toBe(deployer.address.toString());
+    });
+
+    it('should get the proper most recent sender address', async () => {
+        const senderWallet = await blockchain.treasury('sender');
+
+        const sentMessageResult = await mainContract.sendIncrement(senderWallet.getSender(), toNano('0.05'), 1);
+
+        expect(sentMessageResult.transactions).toHaveTransaction({
+            from: senderWallet.address,
+            to: mainContract.address,
+            success: true,
+        });
+
+        const data = await mainContract.getData();
+
+        expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+        expect(data.number).toEqual(2);
+    });
+
+    it('successfully deposits funds', async () => {
+        // test logic is coming
+    });
+    it('should return deposit funds as no command is sent', async () => {
+        // test logic is coming
+    });
+    it('successfully withdraws funds on behalf of owner', async () => {
+        // test logic is coming
+    });
+    it('fails to withdraw funds on behalf of non-owner', async () => {
+        // test logic is coming
+    });
+    it('fails to withdraw funds because lack of balance', async () => {
+        // test logic is coming
     });
 });
--- wrappers/MainContract.ts
+++ wrappers/MainContract.ts
@@ -1,26 +1,42 @@
 import {
-    Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, StateInit,
-    storeStateInit
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Sender,
+    SendMode,
+    StateInit,
+    storeStateInit,
 } from '@ton/core';
 
 export type MainContractConfig = {
     number: number;
     address: Address;
+    owner_address: Address;
 };
 
-export function initConfigToCell(config: MainContractConfig): Cell {
-    return beginCell().storeUint(config.number, 32).storeAddress(config.address).endCell();
+export function mainContractConfigToCell(config: MainContractConfig): Cell {
+    return beginCell()
+        .storeUint(config.number, 32)
+        .storeAddress(config.address)
+        .storeAddress(config.owner_address)
+        .endCell();
 }
 
 export class MainContact implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) { }
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
 
-    static createFromAddress(address: Address) {
+    static async createFromAddress(address: Address) {
         return new MainContact(address);
     }
 
-    static createFromConfig(config: MainContractConfig, code: Cell, workchain = 0) {
-        const data = initConfigToCell(config);
+    static async createFromConfig(config: MainContractConfig, code: Cell, workchain = 0) {
+        const data = mainContractConfigToCell(config);
         const init = { code, data };
         const address = contractAddress(workchain, init);
 
@@ -30,7 +46,6 @@ export class MainContact implements Contract {
     async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
         const msg_body = beginCell()
             .storeUint(1, 32) // OP code
-            .storeUint(0, 32) // increment_by value
             .endCell();
 
         await provider.internal(via, {
@@ -40,12 +55,7 @@ export class MainContact implements Contract {
         });
     }
 
-    async sendIncrement(
-        provider: ContractProvider,
-        sender: Sender,
-        value: bigint,
-        increment_by: number) {
-
+    async sendIncrement(provider: ContractProvider, sender: Sender, value: bigint, increment_by: number) {
         const msg_body = beginCell()
             .storeUint(1, 32) // OP code
             .storeUint(increment_by, 32) // increment_by value
@@ -58,11 +68,31 @@ export class MainContact implements Contract {
         });
     }
 
+    async sendDeposit(provider: ContractProvider, sender: Sender, value: bigint) {
+        const msg_body = beginCell()
+            .storeUint(2, 32) // OP code
+            .endCell();
+
+        await provider.internal(sender, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: msg_body,
+        });
+    }
+
     async getData(provider: ContractProvider) {
-        const { stack } = await provider.get("get_contract_storage_data", []);
+        const { stack } = await provider.get('get_contract_storage_data', []);
         return {
             number: stack.readNumber(),
             recent_sender: stack.readAddress(),
+            owner_address: stack.readAddress(),
+        };
+    }
+
+    async getBalance(provider: ContractProvider) {
+        const { stack } = await provider.get('balance', []);
+        return {
+            number: stack.readNumber(),
         };
     }
 }
